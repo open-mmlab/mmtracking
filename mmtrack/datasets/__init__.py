672068d9f894938a6e1fce8514983753c0b1515b
Jiangmiao Pang <pangjiangmiao@gmail.com>
2020-09-15 18:26:01 +0800

    CocoVideoDataset

--- mmtrack/datasets/__init__.py
+++ mmtrack/datasets/__init__.py
@@ -1,9 +1,11 @@
 from mmdet.datasets.builder import DATASETS, PIPELINES, build_dataset
 
+from .bdd_video_dataset import BDDVideoDataset
 from .builder import build_dataloader
+from .coco_video_dataset import CocoVideoDataset
 from .parsers import CocoVID, DataAPI
 
 __all__ = [
-    'DATASETS', 'PIPELINES', 'build_dataloader', 'build_dataset', 'CocoVID',
-    'DataAPI'
+    'DATASETS', 'PIPELINES', 'build_dataloader', 'build_dataset',
+    'BDDVideoDataset', 'CocoVID', 'CocoVideoDataset', 'DataAPI'
 ]
