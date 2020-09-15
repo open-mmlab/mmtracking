1f2dedf01fe253080417f3966f4528d7f7f36593
Jiangmiao Pang <pangjiangmiao@gmail.com>
2020-09-15 13:50:58 +0800

    rm tracking code for merge

--- mmtrack/datasets/__init__.py
+++ mmtrack/datasets/__init__.py
@@ -2,13 +2,8 @@ from mmdet.datasets.builder import DATASETS, PIPELINES, build_dataset
 
 from .builder import build_dataloader
 from .parsers import CocoVID, DataAPI
-from .pipelines import (LoadMultiImagesFromFile, SeqCollect,
-                        SeqDefaultFormatBundle, SeqLoadAnnotations,
-                        SeqNormalize, SeqPad, SeqRandomFlip, SeqResize)
 
 __all__ = [
     'DATASETS', 'PIPELINES', 'build_dataloader', 'build_dataset', 'CocoVID',
-    'DataAPI', 'LoadMultiImagesFromFile', 'SeqLoadAnnotations', 'SeqResize',
-    'SeqNormalize', 'SeqRandomFlip', 'SeqPad', 'SeqDefaultFormatBundle',
-    'SeqCollect'
+    'DataAPI'
 ]
--- mmtrack/datasets/pipelines/__init__.py
+++
@@ -1,9 +0,0 @@
-from .formatting import VideoCollect, SeqCollect, SeqDefaultFormatBundle
-from .loading import LoadMultiImagesFromFile, SeqLoadAnnotations
-from .transforms import SeqNormalize, SeqPad, SeqRandomFlip, SeqResize
-
-__all__ = [
-    'LoadMultiImagesFromFile', 'SeqLoadAnnotations', 'SeqResize',
-    'SeqNormalize', 'SeqRandomFlip', 'SeqPad', 'SeqDefaultFormatBundle',
-    'SeqCollect', 'VideoCollect'
-]
