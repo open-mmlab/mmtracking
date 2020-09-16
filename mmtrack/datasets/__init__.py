6418693c259fdab29f49cedd0e630b21333e3716
Jiangmiao Pang <pangjiangmiao@gmail.com>
2020-09-16 21:19:35 +0800

    pipelines

--- mmtrack/datasets/__init__.py
+++ mmtrack/datasets/__init__.py
@@ -1,9 +1,10 @@
-from mmdet.datasets.builder import DATASETS, PIPELINES, build_dataset
+from mmdet.datasets.builder import DATASETS, build_dataset
 
 from .bdd_video_dataset import BDDVideoDataset
 from .builder import build_dataloader
 from .coco_video_dataset import CocoVideoDataset
 from .parsers import CocoVID, DataAPI
+from .pipelines import PIPELINES
 
 __all__ = [
     'DATASETS', 'PIPELINES', 'build_dataloader', 'build_dataset',
---
+++ mmtrack/datasets/pipelines/loading.py
@@ -0,0 +1,49 @@
+from mmdet.datasets.builder import PIPELINES
+from mmdet.datasets.pipelines import LoadAnnotations, LoadImageFromFile
+
+
+@PIPELINES.register_module()
+class LoadMultiImagesFromFile(LoadImageFromFile):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def __call__(self, results):
+        outs = []
+        for _results in results:
+            _results = super().__call__(_results)
+            outs.append(_results)
+        return outs
+
+
+@PIPELINES.register_module()
+class SeqLoadAnnotations(LoadAnnotations):
+
+    def __init__(self, with_track=False, *args, **kwargs):
+        # TODO: name
+        super().__init__(*args, **kwargs)
+        self.with_track = with_track
+
+    def _load_ins_ids(self, results):
+        """Private function to load label annotations.
+
+        Args:
+            results (dict): Result dict from :obj:`mmdet.CustomDataset`.
+
+        Returns:
+            dict: The dict contains loaded label annotations.
+        """
+
+        results['gt_match_indices'] = results['ann_info'][
+            'match_indices'].copy()
+
+        return results
+
+    def __call__(self, results):
+        outs = []
+        for _results in results:
+            _results = super().__call__(_results)
+            if self.with_track:
+                _results = self._load_ins_ids(_results)
+            outs.append(_results)
+        return outs
