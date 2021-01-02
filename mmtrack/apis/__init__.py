87ba3071d419a0a33bf54fcab131499de8fa3c18
GT9505 <gt950513@mail.ustc.edu.cn>
2021-01-02 15:06:33 +0800

    add sot demo

--- mmtrack/apis/__init__.py
+++ mmtrack/apis/__init__.py
@@ -1,4 +1,8 @@
+from .sot_inference import init_sot_model, sot_inference
 from .test import multi_gpu_test, single_gpu_test
 from .train import train_model
 
-__all__ = ['multi_gpu_test', 'single_gpu_test', 'train_model']
+__all__ = [
+    'multi_gpu_test', 'single_gpu_test', 'train_model', 'init_sot_model',
+    'sot_inference'
+]
