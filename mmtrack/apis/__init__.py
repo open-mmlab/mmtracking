4865eb56eb9c6189e220ff8bf29622fc1a86e102
GT9505 <gt950513@mail.ustc.edu.cn>
2021-01-02 15:20:52 +0800

    add vid demo

--- mmtrack/apis/__init__.py
+++ mmtrack/apis/__init__.py
@@ -1,8 +1,9 @@
 from .sot_inference import init_sot_model, sot_inference
 from .test import multi_gpu_test, single_gpu_test
 from .train import train_model
+from .vid_inference import init_vid_model, vid_inference
 
 __all__ = [
     'multi_gpu_test', 'single_gpu_test', 'train_model', 'init_sot_model',
-    'sot_inference'
+    'sot_inference', 'init_vid_model', 'vid_inference'
 ]
