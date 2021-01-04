204779a76d645314eb5d457b4fc2d9969ee9e83e
GT9505 <gongtao950513@gmail.com>
2021-01-04 18:59:48 +0800

    Fix demo (#62)
    * change demo of sot and vid
    
    * update demo of sot and vid
    
    * update quick_run.md
    
    * update quick_run.md

--- demo/demo_sot.py
+++ demo/demo_sot.py
@@ -1,4 +1,3 @@
-import os
 from argparse import ArgumentParser
 
 import cv2
@@ -8,14 +7,12 @@ from mmtrack.apis import inference_sot, init_model
 
 def main():
     parser = ArgumentParser()
-    parser.add_argument('video', help='Video file')
     parser.add_argument('config', help='Config file')
-    parser.add_argument('checkpoint', help='Checkpoint file')
+    parser.add_argument('--input', help='input video file')
+    parser.add_argument('--output', help='output video file (mp4 format)')
+    parser.add_argument('--checkpoint', help='Checkpoint file')
     parser.add_argument(
-        '--out-video-root',
-        default='',
-        help='Root of the output video file. '
-        'Default not saving the visualization video.')
+        '--device', default='cuda:0', help='Device used for inference')
     parser.add_argument(
         '--show',
         action='store_true',
@@ -25,28 +22,21 @@ def main():
         '--color', default=(0, 255, 0), help='Color of tracked bbox lines.')
     parser.add_argument(
         '--thickness', default=3, help='Thickness of bbox lines.')
-    parser.add_argument(
-        '--device', default='cuda:0', help='Device used for inference')
     args = parser.parse_args()
 
     # build the model from a config file and a checkpoint file
     model = init_model(args.config, args.checkpoint, device=args.device)
 
-    cap = cv2.VideoCapture(args.video)
+    cap = cv2.VideoCapture(args.input)
 
-    if args.out_video_root == '':
-        save_out_video = False
-    else:
-        os.makedirs(args.out_video_root, exist_ok=True)
+    if args.output is not None:
         save_out_video = True
 
         fps = cap.get(cv2.CAP_PROP_FPS)
         size = (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)),
                 int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
         fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-        out_video_path = os.path.join(args.out_video_root,
-                                      f'vis_{os.path.basename(args.video)}')
-        videoWriter = cv2.VideoWriter(out_video_path, fourcc, fps, size)
+        videoWriter = cv2.VideoWriter(args.output, fourcc, fps, size)
 
     frame_id = 0
     while (cap.isOpened()):
@@ -55,7 +45,7 @@ def main():
             break
 
         if frame_id == 0:
-            init_bbox = list(cv2.selectROI(args.video, frame, False, False))
+            init_bbox = list(cv2.selectROI(args.input, frame, False, False))
             # convert (x1, y1, w, h) to (x1, y1, x2, y2)
             init_bbox[2] += init_bbox[0]
             init_bbox[3] += init_bbox[1]
@@ -74,7 +64,7 @@ def main():
             videoWriter.write(frame)
 
         if args.show:
-            cv2.imshow(args.video, frame)
+            cv2.imshow(args.input, frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
