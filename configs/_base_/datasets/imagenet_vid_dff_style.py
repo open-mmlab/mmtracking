a89d4905f0a27099807c3590415ef9cfe1213659
GT9505 <gt950513@mail.ustc.edu.cn>
2021-01-04 10:18:12 +0800

    fix a bug in vid configs

--- configs/_base_/datasets/imagenet_vid_dff_style.py
+++ configs/_base_/datasets/imagenet_vid_dff_style.py
@@ -1,6 +1,6 @@
 # dataset settings
 dataset_type = 'ImagenetVIDDataset'
-data_root = 'data/ILSVRC'
+data_root = 'data/ILSVRC/'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 train_pipeline = [
