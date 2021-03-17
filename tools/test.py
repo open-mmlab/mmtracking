dfb86a6e5edb19d4e318c3b90c4243916c3b3a6e
GT9505 <gt950513@mail.ustc.edu.cn>
2021-03-17 17:03:40 +0800

    import fp16 api from mmcv

--- tools/test.py
+++ tools/test.py
@@ -6,8 +6,8 @@ import torch
 from mmcv import Config, DictAction
 from mmcv.cnn import fuse_conv_bn
 from mmcv.parallel import MMDataParallel, MMDistributedDataParallel
-from mmcv.runner import get_dist_info, init_dist, load_checkpoint
-from mmdet.core import wrap_fp16_model
+from mmcv.runner import (get_dist_info, init_dist, load_checkpoint,
+                         wrap_fp16_model)
 from mmdet.datasets import build_dataset
 
 
