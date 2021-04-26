6396ec6e74b3fa06a7af53a2e59d3a54dd06b3f0
ToumaKazusa3 <ssjxjx@mail.ustc.edu.cn>
2021-04-27 00:24:06 +0800

    fix a bug

---
+++ configs/mot/deepsort/myself_sort_faster-rcnn_fpn_4e_mot17-private-half_job5.py
@@ -0,0 +1,29 @@
+_base_ = [
+    '../../_base_/models/faster_rcnn_r50_fpn.py',
+    '../../_base_/datasets/mot_challenge.py', '../../_base_/default_runtime.py'
+]
+model = dict(
+    type='DeepSORT',
+    pretrains=dict(
+        detector=  # noqa: E251
+        'work_dirs/detector/faster-rcnn_r50_fpn_4e_mot17-half_job5/latest.pth'  # noqa: E501
+    ),
+    detector=dict(
+        rpn_head=dict(bbox_coder=dict(clip_border=False)),
+        roi_head=dict(
+            bbox_head=dict(bbox_coder=dict(
+                clip_border=False), num_classes=1))),
+    motion=dict(type='KalmanFilter', center_only=False),
+    tracker=dict(
+        type='SortTracker', obj_score_thr=0.5, match_iou_thr=0.5, reid=None))
+# learning policy
+lr_config = dict(
+    policy='step',
+    warmup='linear',
+    warmup_iters=100,
+    warmup_ratio=1.0 / 100,
+    step=[3])
+# runtime settings
+total_epochs = 4
+evaluation = dict(metric=['bbox', 'track'], interval=1)
+search_metrics = ['MOTA', 'IDF1', 'FN', 'FP', 'IDs', 'MT', 'ML']
