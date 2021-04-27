303d72fd316feea5d7ffbcd55adaa74e53c4be22
ToumaKazusa3 <ssjxjx@mail.ustc.edu.cn>
2021-04-27 13:41:50 +0800

    Add new datasets

---
+++ configs/mot/deepsort/sort_faster-rcnn_fpn_4e_mot16-public-half.py
@@ -0,0 +1,8 @@
+_base_ = ['./sort_faster-rcnn_fpn_4e_mot17-public-half']
+data_root = 'data/MOT16/'
+data = dict(
+    val=dict(
+        detection_file=data_root + 'annotations/half-val_detections.pkl'),
+    test=dict(
+        detection_file=data_root + 'annotations/half-val_detections.pkl'))
+
---
+++ configs/mot/deepsort/sort_faster-rcnn_fpn_4e_mot20-public-half.py
@@ -0,0 +1,7 @@
+_base_ = ['./sort_faster-rcnn_fpn_4e_mot17-public-half']
+data_root = 'data/MOT20/'
+data = dict(
+    val=dict(
+        detection_file=data_root + 'annotations/half-val_detections.pkl'),
+    test=dict(
+        detection_file=data_root + 'annotations/half-val_detections.pkl'))
