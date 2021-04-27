daa816eb0128bd5ed4d80380e52bb35ae0a0e99d
ToumaKazusa3 <ssjxjx@mail.ustc.edu.cn>
2021-04-27 14:19:01 +0800

    Add new datasets

--- configs/mot/deepsort/sort_faster-rcnn_fpn_4e_mot16-public-half.py
+++ configs/mot/deepsort/sort_faster-rcnn_fpn_4e_mot16-public-half.py
@@ -1,4 +1,9 @@
-_base_ = ['./sort_faster-rcnn_fpn_4e_mot17-public-half']
+_base_ = ['./sort_faster-rcnn_fpn_4e_mot17-public-half.py']
+model = dict(
+    pretrains=dict(
+        detector=  # noqa: E251
+        'work_dirs/detector/faster-rcnn_r50_fpn_4e_mot16-half/latest.pth'  # noqa: E501
+    ))
 data_root = 'data/MOT16/'
 data = dict(
     val=dict(
