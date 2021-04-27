9e4800d7842efe9f539e4f57ba282ae936db3708
ToumaKazusa3 <ssjxjx@mail.ustc.edu.cn>
2021-04-27 15:08:29 +0800

    Add new datasets

---
+++ configs/mot/deepsort/sort_faster-rcnn_fpn_4e_mot16-private-half.py
@@ -0,0 +1,16 @@
+_base_ = ['./sort_faster-rcnn_fpn_4e_mot17-private-half.py']
+model = dict(
+    pretrains=dict(
+        detector=  # noqa: E251
+        'work_dirs/detector/faster-rcnn_r50_fpn_4e_mot15-half/latest.pth'  # noqa: E501
+    ))
+data_root = 'data/MOT16/'
+data = dict(
+    val=dict(
+        ann_file=data_root + 'annotations/half-val_cocoformat.json',
+        img_prefix=data_root + 'train',
+        detection_file=data_root + 'annotations/half-val_detections.pkl'),
+    test=dict(
+        ann_file=data_root + 'annotations/half-val_cocoformat.json',
+        img_prefix=data_root + 'train',
+        detection_file=data_root + 'annotations/half-val_detections.pkl'))
