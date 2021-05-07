fe103e1ff613ae22367c33fc4762c6ec30ae0bfa
ToumaKazusa3 <ssjxjx@mail.ustc.edu.cn>
2021-05-07 15:15:06 +0800

    fix a bug in configs

--- configs/mot/tracktor/tracktor_faster-rcnn_r50_fpn_4e_mot16-private.py
+++ configs/mot/tracktor/tracktor_faster-rcnn_r50_fpn_4e_mot16-private.py
@@ -1,4 +1,4 @@
-_base_ = ['./tracktor_faster-rcnn_fpn_4e_mot17-private.py']
+_base_ = ['./tracktor_faster-rcnn_r50_fpn_4e_mot17-private.py']
 model = dict(
     pretrains=dict(
         detector=  # noqa: E251
