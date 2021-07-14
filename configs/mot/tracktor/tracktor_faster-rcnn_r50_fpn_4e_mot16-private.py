e22fcc01c1da403555d3287e79873a081b889075
ToumaKazusa3 <ssjxjx@mail.ustc.edu.cn>
2021-07-14 10:39:23 +0800

    [Feature] Support MOT15 MOT16 and MOT20

---
+++ configs/mot/tracktor/tracktor_faster-rcnn_r50_fpn_4e_mot16-private.py
@@ -0,0 +1,14 @@
+_base_ = ['./tracktor_faster-rcnn_r50_fpn_4e_mot16-private-half.py']
+model = dict(
+    pretrains=dict(
+        detector=  # noqa: E251
+        'work_dirs/publish_model/faster-rcnn_r50_fpn_4e_mot16-ccb2ff52.pth'  # noqa: E501
+    ))
+data_root = 'data/MOT16/'
+test_set = 'train'
+data = dict(
+    train=dict(ann_file=data_root + 'annotations/train_cocoformat.json'),
+    val=dict(ann_file=data_root + 'annotations/train_cocoformat.json'),
+    test=dict(
+        ann_file=data_root + f'annotations/{test_set}_cocoformat.json',
+        img_prefix=data_root + test_set))
