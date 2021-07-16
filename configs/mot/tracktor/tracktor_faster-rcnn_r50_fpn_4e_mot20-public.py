f9719eefd738a1a8196dd5e943290bcd5dcfc5e4
ToumaKazusa3 <ssjxjx@mail.ustc.edu.cn>
2021-07-16 12:25:04 +0800

    add mot20

---
+++ configs/mot/tracktor/tracktor_faster-rcnn_r50_fpn_4e_mot20-public.py
@@ -0,0 +1,17 @@
+_base_ = ['./tracktor_faster-rcnn_r50_fpn_4e_mot20-public-half.py']
+model = dict(
+    pretrains=dict(
+        detector=  # noqa: E251
+        'work_dirs/publish_model/faster-rcnn_r50_fpn_8e_mot20-ef875499.pth'  # noqa: E501
+    ))
+data_root = 'data/MOT20/'
+test_set = 'train'
+data = dict(
+    train=dict(ann_file=data_root + 'annotations/train_cocoformat.json'),
+    val=dict(
+        ann_file=data_root + 'annotations/train_cocoformat.json',
+        detection_file=data_root + 'annotations/train_detections.pkl'),
+    test=dict(
+        ann_file=data_root + f'annotations/{test_set}_cocoformat.json',
+        img_prefix=data_root + test_set,
+        detection_file=data_root + f'annotations/{test_set}_detections.pkl'))
