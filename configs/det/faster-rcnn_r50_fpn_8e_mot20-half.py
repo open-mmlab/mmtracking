189fb25a1465f3642b37764d5aa84b89f67bf2b1
ToumaKazusa3 <ssjxjx@mail.ustc.edu.cn>
2021-07-28 13:43:04 +0800

    fix a bug

--- configs/det/faster-rcnn_r50_fpn_8e_mot20-half.py
+++ configs/det/faster-rcnn_r50_fpn_8e_mot20-half.py
@@ -5,10 +5,9 @@ _base_ = [
 ]
 model = dict(
     detector=dict(
-        rpn_head=dict(bbox_coder=dict(clip_border=False)),
+        rpn_head=dict(bbox_coder=dict(clip_border=True)),
         roi_head=dict(
-            bbox_head=dict(bbox_coder=dict(
-                clip_border=False), num_classes=1))))
+            bbox_head=dict(bbox_coder=dict(clip_border=True), num_classes=1))))
 # data
 data_root = 'data/MOT20/'
 data = dict(
