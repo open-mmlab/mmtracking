a870e62abb48be0426ed0d46b73ea79d3b793f2f
zhangjingwei <zhangjingwei@sensetime.com>
2021-08-19 17:09:47 +0800

    del int_wh

--- mmtrack/core/anchor/sot_anchor_generator.py
+++ mmtrack/core/anchor/sot_anchor_generator.py
@@ -11,10 +11,10 @@ class SiameseRPNAnchorGenerator(AnchorGenerator):
     for detailed docstring.
     """
 
-    def __init__(self, strides, int_wh=False, **kwargs):
+    def __init__(self, strides, *args, **kwargs):
         assert len(strides) == 1, 'only support one feature map level'
-        self.int_wh = int_wh
-        super(SiameseRPNAnchorGenerator, self).__init__(strides, **kwargs)
+        super(SiameseRPNAnchorGenerator,
+              self).__init__(strides, *args, **kwargs)
 
     def gen_2d_hanning_windows(self, featmap_sizes, device='cuda'):
         """Generate 2D hanning window.
@@ -66,17 +66,12 @@ class SiameseRPNAnchorGenerator(AnchorGenerator):
 
         h_ratios = torch.sqrt(ratios)
         w_ratios = 1 / h_ratios
-        wr = w * w_ratios
-        hr = h * h_ratios
-        if self.int_wh:
-            wr = wr.long()
-            hr = hr.long()
         if self.scale_major:
-            ws = (wr[:, None] * scales[None, :]).view(-1)
-            hs = (hr[:, None] * scales[None, :]).view(-1)
+            ws = ((w * w_ratios[:, None]).long() * scales[None, :]).view(-1)
+            hs = ((h * h_ratios[:, None]).long() * scales[None, :]).view(-1)
         else:
-            ws = (wr[None, :] * scales[:, None]).view(-1)
-            hs = (hr[None, :] * scales[:, None]).view(-1)
+            ws = ((w * w_ratios[None, :]).long() * scales[:, None]).view(-1)
+            hs = ((h * h_ratios[None, :]).long() * scales[:, None]).view(-1)
 
         # use float anchor and the anchor's center is aligned with the
         # pixel center
