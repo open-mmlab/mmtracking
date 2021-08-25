8016b903aceb21e3e6a97d72b6f17efef1c86562
Tao Gong <gongtao950513@gmail.com>
2021-08-25 15:16:02 +0800

    Release code of AAAI 2021 paper. "Temporal ROI Align for Video Object Recognition" (#247)
    * release the code of Temporal RoI Align
    
    * add docstring for troi code
    
    * add unittest for troi code
    
    * change README.md, metafile.yml, model_zoo.md and model-index.yml
    
    * tiny changes of README.md
    
    * tiny changes of metafile.yml
    
    * tiny changes of README.md
    
    * update based 1-st comments
    
    * update based on 2-nd comments

---
+++ mmtrack/models/roi_heads/roi_extractors/single_level_roi_extractor.py
@@ -0,0 +1,19 @@
+from mmcv.runner import force_fp32
+from mmdet.models.builder import ROI_EXTRACTORS
+from mmdet.models.roi_heads.roi_extractors import \
+    SingleRoIExtractor as _SingleRoIExtractor
+
+
+@ROI_EXTRACTORS.register_module(force=True)
+class SingleRoIExtractor(_SingleRoIExtractor):
+    """Extract RoI features from a single level feature map.
+
+    This Class is the same as `SingleRoIExtractor` from
+    `mmdet.models.roi_heads.roi_extractors` except for using `**kwargs` to
+    accept external arguments.
+    """
+
+    @force_fp32(apply_to=('feats', ), out_fp16=True)
+    def forward(self, feats, rois, roi_scale_factor=None, **kwargs):
+        """Forward function."""
+        return super().forward(feats, rois, roi_scale_factor)
---
+++ tests/test_models/test_roi_heads/test_roi_extractors/test_temporal_roi_align.py
@@ -0,0 +1,26 @@
+import torch
+
+from mmtrack.models.roi_heads.roi_extractors import TemporalRoIAlign
+
+
+def test_temporal_roi_align():
+    """Test Temporal RoI Align."""
+    temporal_roi_align_config = dict(
+        num_most_similar_points=2,
+        num_temporal_attention_blocks=4,
+        roi_layer=dict(type='RoIAlign', output_size=7, sampling_ratio=2),
+        out_channels=256,
+        featmap_strides=[16])
+    self = TemporalRoIAlign(**temporal_roi_align_config)
+
+    feats = (torch.rand((1, 256, 50, 84)), )
+    ref_feats = (feats[0].repeat((2, 1, 1, 1)), )
+    rois = torch.tensor([[0.0000, 587.8285, 52.1405, 886.2484, 341.5644]])
+
+    # test when ref_feats is not None
+    roi_feats = self(feats, rois, ref_feats=ref_feats)
+    assert roi_feats.shape == torch.Size([1, 256, 7, 7])
+
+    # test when ref_feats is None
+    roi_feats = self(feats, rois, ref_feats=None)
+    assert roi_feats.shape == torch.Size([1, 256, 7, 7])
