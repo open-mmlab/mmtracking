88a948b136e7e336d38f63588f622cfc49dbef57
zhangjingwei <zhangjingwei@sensetime.com>
2021-09-15 11:37:41 +0800

    fix bug about boundary clipping

--- mmtrack/models/sot/siamrpn.py
+++ mmtrack/models/sot/siamrpn.py
@@ -173,13 +173,14 @@ class SiamRPN(BaseSingleObjectTracker):
             align_corners=False)
         return crop_img
 
-    # TODO fix the bug about boundary clipping
     def _bbox_clip(self, bbox, img_h, img_w):
         """Clip the bbox with [cx, cy, w, h] format."""
-        bbox[0] = bbox[0].clamp(0., img_w)
-        bbox[1] = bbox[1].clamp(0., img_h)
-        bbox[2] = bbox[2].clamp(10., img_w)
-        bbox[3] = bbox[3].clamp(10., img_h)
+        corner_bbox = bbox_cxcywh_to_xyxy(bbox)
+        corner_bbox[0] = corner_bbox[0].clamp(0., img_w)
+        corner_bbox[1] = corner_bbox[1].clamp(0., img_h)
+        corner_bbox[2] = corner_bbox[2].clamp(0., img_w)
+        corner_bbox[3] = corner_bbox[3].clamp(0., img_h)
+        bbox = bbox_xyxy_to_cxcywh(corner_bbox)
         return bbox
 
     def init(self, img, bbox):
