d268ef2f760993c620c50b6ffee7fb91330e6565
JingweiZhang12 <zhangjingwei@pjlab.org.cn>
2021-11-12 22:05:18 +0800

    fix unittest

--- mmtrack/core/bbox/iou_calculators/__init__.py
+++ mmtrack/core/bbox/iou_calculators/__init__.py
@@ -1,4 +1,4 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from .region_iou import calculate_region_overlap
+from .region_iou_calculator import calculate_region_overlap
 
 __all__ = ['calculate_region_overlap']
