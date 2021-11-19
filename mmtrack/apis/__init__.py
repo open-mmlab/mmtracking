621b68a86ecbd1d96f6afbb6ff8bfa74c10adf59
Tao Gong <gongtao950513@gmail.com>
2021-11-19 10:18:02 +0800

    Set a random seed when the user does not set a seed

--- mmtrack/apis/__init__.py
+++ mmtrack/apis/__init__.py
@@ -1,9 +1,9 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .inference import inference_mot, inference_sot, inference_vid, init_model
 from .test import multi_gpu_test, single_gpu_test
-from .train import train_model
+from .train import init_random_seed, train_model
 
 __all__ = [
     'init_model', 'multi_gpu_test', 'single_gpu_test', 'train_model',
-    'inference_mot', 'inference_sot', 'inference_vid'
+    'inference_mot', 'inference_sot', 'inference_vid', 'init_random_seed'
 ]
