f661b871f8c2ff1d6c45e6a95932f277144cf3de
JingweiZhang12 <zhangjingwei@sensetime.com>
2021-11-22 11:12:27 +0800

    fix otb convert json bug

--- tools/convert_datasets/otb100/otb2coco.py
+++ tools/convert_datasets/otb100/otb2coco.py
@@ -84,7 +84,7 @@ def convert_otb100(otb, ann_dir, save_dir):
                     video_id=records['vid_id'])
                 otb['images'].append(image)
 
-                bbox = list(map(int, re.findall(r'\d+', gt_bbox)))
+                bbox = list(map(int, re.findall(r'-?\d+', gt_bbox)))
                 assert len(bbox) == 4
                 anno_dict = dict(
                     id=records['ann_id'],
