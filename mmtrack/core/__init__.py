0d066fa71ba49856ad6d2bbedda47c3082833797
Tao Gong <gt950513@mail.ustc.edu.cn>
2021-12-16 07:10:08 -0600

    fix typos

--- mmtrack/core/__init__.py
+++ mmtrack/core/__init__.py
@@ -2,7 +2,7 @@
 from .anchor import *  # noqa: F401, F403
 from .bbox import *  # noqa: F401, F403
 from .evaluation import *  # noqa: F401, F403
-from .hooks import *  # noqa: F401, F403
+from .hook import *  # noqa: F401, F403
 from .motion import *  # noqa: F401, F403
 from .optimizer import *  # noqa: F401, F403
 from .track import *  # noqa: F401, F403
