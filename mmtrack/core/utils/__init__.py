67bf8d2aee224f79a455cf9ef7426ab91e04cda5
Tao Gong <gt950513@mail.ustc.edu.cn>
2022-01-17 14:54:33 +0800

    Accelerating training speed (#395)

--- mmtrack/core/utils/__init__.py
+++ mmtrack/core/utils/__init__.py
@@ -1,5 +1,8 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .image import crop_image
+from .misc import setup_multi_processes
 from .visualization import imshow_mot_errors, imshow_tracks
 
-__all__ = ['crop_image', 'imshow_tracks', 'imshow_mot_errors']
+__all__ = [
+    'crop_image', 'imshow_tracks', 'imshow_mot_errors', 'setup_multi_processes'
+]
