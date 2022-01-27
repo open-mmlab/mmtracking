bdeea0cb9db3c0fa15f9b714b303b30f0567b151
JingweiZhang12 <zjw18@mails.tsinghua.edu.cn>
2022-01-27 16:25:11 +0800

    add docstring and unittest

--- mmtrack/datasets/dataset_wrapers.py
+++ mmtrack/datasets/dataset_wrapers.py
@@ -9,30 +9,34 @@ from mmdet.datasets.builder import build_dataset
 @DATASETS.register_module()
 class RandomSampleConcatDataset(ConcatDataset):
     """A wrapper of concatenated dataset. Support randomly sampling one dataset
-        from concatenated datasets.
+        from concatenated datasets and then geting samples from the sampled
+        dataset.
 
     Args:
-        dataset_cfgs (list[dict]): The list contains the configs of several
-            datasets.
-        datasets_sampling_prob (list): The list contains the sampling
-            probilities of each dataset.
+        dataset_cfgs (list[dict]): The list contains all configs of
+            concatenated datasets.
+        dataset_sampling_weights (list): The list contains the sampling
+            weights of each dataset.
     """
 
-    def __init__(self, dataset_cfgs, datasets_sampling_prob=None):
+    def __init__(self, dataset_cfgs, dataset_sampling_weights=None):
         datasets = [build_dataset(cfg) for cfg in dataset_cfgs]
         super().__init__(datasets)
-        if datasets_sampling_prob is None:
-            self.datasets_sampling_prob = [1 / len(datasets)] * len(datasets)
+        if dataset_sampling_weights is None:
+            self.dataset_sampling_probs = [1 / len(datasets)] * len(datasets)
         else:
-            prob_total = sum(datasets_sampling_prob)
-            self.datasets_sampling_prob = [
-                x / prob_total for x in datasets_sampling_prob
+            prob_total = sum(dataset_sampling_weights)
+            self.dataset_sampling_probs = [
+                x / prob_total for x in dataset_sampling_weights
             ]
 
     def __getitem__(self, ind):
+        """Random sampling a dataset and get samples from this dataset.
+            Actually, the input 'ind' is not used in 'dataset'.
+        """
         while True:
             dataset = random.choices(self.datasets,
-                                     self.datasets_sampling_prob)[0]
+                                     self.dataset_sampling_probs)[0]
             results = dataset[ind]
             if results is not None:
                 return results
