f92c2d4d240002c61699fb7a68adf9957a7b0db8
Tao Gong <gt950513@mail.ustc.edu.cn>
2022-01-28 15:53:27 +0800

    Update __init__.py

--- mmtrack/datasets/__init__.py
+++ mmtrack/datasets/__init__.py
@@ -4,7 +4,7 @@ from mmdet.datasets.builder import DATASETS, build_dataset
 from .base_sot_dataset import BaseSOTDataset
 from .builder import build_dataloader
 from .coco_video_dataset import CocoVideoDataset
-from .dataset_wrapers import RandomSampleConcatDataset
+from .dataset_wrappers import RandomSampleConcatDataset
 from .got10k_dataset import GOT10kDataset
 from .imagenet_vid_dataset import ImagenetVIDDataset
 from .lasot_dataset import LaSOTDataset
