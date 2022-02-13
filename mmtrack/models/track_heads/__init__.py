c15ced8644ae13a11bd64f7cbad2d685fba7e55b
方云浩 <PJLAB\fangyunhao@shai14001225l.pjlab.org>
2022-02-13 14:17:07 +0800

    add track head.

--- mmtrack/models/track_heads/__init__.py
+++ mmtrack/models/track_heads/__init__.py
@@ -1,6 +1,10 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+from .quasi_dense_embed_head import QuasiDenseEmbedHead
 from .roi_embed_head import RoIEmbedHead
 from .roi_track_head import RoITrackHead
 from .siamese_rpn_head import CorrelationHead, SiameseRPNHead
 
-__all__ = ['CorrelationHead', 'SiameseRPNHead', 'RoIEmbedHead', 'RoITrackHead']
+__all__ = [
+    'CorrelationHead', 'SiameseRPNHead', 'RoIEmbedHead', 'RoITrackHead',
+    'QuasiDenseEmbedHead'
+]
