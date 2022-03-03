e6e5b7807b59b2b84ef58ccb0abe091738f61ac2
方云浩 <PJLAB\fangyunhao@shai14001225l.pjlab.org>
2022-03-03 16:49:38 +0800

    modify configs' structure.

--- configs/mot/qdtrack/qdtrack_faster_rcnn_r101_fpn_4e_mot17.py
+++ configs/mot/qdtrack/qdtrack_faster_rcnn_r101_fpn_4e_mot17.py
@@ -1,100 +1,64 @@
-_base_ = './qdtrack_faster_rcnn_r50_fpn.py'
-
+_base_ = [
+    '../../_base_/models/faster_rcnn_r50_fpn.py',
+    '../../_base_/datasets/mot_challenge.py', '../../_base_/default_runtime.py'
+]
 model = dict(
+    type='QDTrack',
     detector=dict(
-        backbone=dict(norm_cfg=dict(requires_grad=False), style='caffe'),
+        backbone=dict(
+            norm_cfg=dict(requires_grad=False),
+            style='caffe',
+            init_cfg=dict(
+                type='Pretrained', checkpoint='torchvision://resnet101')),
         rpn_head=dict(bbox_coder=dict(clip_border=False)),
         roi_head=dict(
-            bbox_head=dict(bbox_coder=dict(
-                clip_border=False), num_classes=1))),
-    track_head=dict(train_cfg=dict(assigner=dict(neg_iou_thr=0.5))))
-
+            bbox_head=dict(
+                loss_bbox=dict(type='L1Loss', loss_weight=1.0),
+                bbox_coder=dict(clip_border=False),
+                num_classes=1))),
+    track_head=dict(
+        type='QuasiDenseTrackHead',
+        roi_extractor=dict(
+            type='SingleRoIExtractor',
+            roi_layer=dict(type='RoIAlign', output_size=7, sampling_ratio=0),
+            out_channels=256,
+            featmap_strides=[4, 8, 16, 32]),
+        embed_head=dict(
+            type='QuasiDenseEmbedHead',
+            num_convs=4,
+            num_fcs=1,
+            embed_channels=256,
+            norm_cfg=dict(type='GN', num_groups=32),
+            loss_track=dict(type='MultiPosCrossEntropyLoss', loss_weight=0.25),
+            loss_track_aux=dict(
+                type='L2Loss',
+                neg_pos_ub=3,
+                pos_margin=0,
+                neg_margin=0.1,
+                hard_mining=True,
+                loss_weight=1.0)),
+        loss_bbox=dict(type='L1Loss', loss_weight=1.0),
+        train_cfg=dict(
+            assigner=dict(
+                type='MaxIoUAssigner',
+                pos_iou_thr=0.7,
+                neg_iou_thr=0.5,
+                min_pos_iou=0.5,
+                match_low_quality=False,
+                ignore_iof_thr=-1),
+            sampler=dict(
+                type='CombinedSampler',
+                num=256,
+                pos_fraction=0.5,
+                neg_pos_ub=3,
+                add_gt_as_proposals=True,
+                pos_sampler=dict(type='InstanceBalancedPosSampler'),
+                neg_sampler=dict(type='RandomSampler')))))
 img_norm_cfg = dict(
     mean=[103.530, 116.280, 123.675], std=[1.0, 1.0, 1.0], to_rgb=False)
-train_pipeline = [
-    dict(type='LoadMultiImagesFromFile', to_float32=True),
-    dict(type='SeqLoadAnnotations', with_bbox=True, with_track=True),
-    dict(
-        type='SeqResize',
-        img_scale=(1088, 1088),
-        share_params=True,
-        ratio_range=(0.8, 1.2),
-        keep_ratio=True,
-        bbox_clip_border=False),
-    dict(type='SeqPhotoMetricDistortion', share_params=True),
-    dict(
-        type='SeqRandomCrop',
-        share_params=False,
-        crop_size=(1088, 1088),
-        bbox_clip_border=False),
-    dict(type='SeqRandomFlip', share_params=True, flip_ratio=0.5),
-    dict(type='SeqNormalize', **img_norm_cfg),
-    dict(type='SeqPad', size_divisor=32),
-    dict(type='MatchInstances', skip_nomatch=True),
-    dict(
-        type='VideoCollect',
-        keys=['img', 'gt_bboxes', 'gt_labels', 'gt_match_indices'],
-    ),
-    dict(type='SeqDefaultFormatBundle', ref_prefix='ref')
-]
-test_pipeline = [
-    dict(type='LoadImageFromFile'),
-    dict(
-        type='MultiScaleFlipAug',
-        img_scale=(1088, 1088),
-        flip=False,
-        transforms=[
-            dict(type='Resize', keep_ratio=True),
-            dict(type='RandomFlip'),
-            dict(type='Normalize', **img_norm_cfg),
-            dict(type='Pad', size_divisor=32),
-            dict(type='ImageToTensor', keys=['img']),
-            dict(type='VideoCollect', keys=['img'])
-        ])
-]
-
-dataset_type = 'MOTChallengeDataset'
-data_root = 'data/MOT17/'
-data = dict(
-    samples_per_gpu=1,
-    workers_per_gpu=0,
-    train=dict(
-        type=dataset_type,
-        classes=['pedestrian'],
-        visibility_thr=-1,
-        # track_visibility_thr=-1,
-        ann_file='data/MOT17/annotations/half-train_cocoformat.json',
-        img_prefix='data/MOT17/train/',
-        ref_img_sampler=dict(num_ref_imgs=1, frame_range=10, method='uniform'),
-        pipeline=train_pipeline),
-    val=dict(
-        type=dataset_type,
-        ann_file='data/MOT17/annotations/half-val_cocoformat.json',
-        classes=['pedestrian'],
-        img_prefix='data/MOT17/train/',
-        ref_img_sampler=None,
-        pipeline=test_pipeline),
-    test=dict(
-        type=dataset_type,
-        ann_file='data/MOT17/annotations/half-val_cocoformat.json',
-        img_prefix='data/MOT17/train/',
-        classes=['pedestrian'],
-        ref_img_sampler=None,
-        pipeline=test_pipeline))
-
-optimizer = dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.0001)
+# optimizer && learning policy
 optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
 lr_config = dict(policy='step', step=[3])
-checkpoint_config = dict(interval=1)
-log_config = dict(
-    interval=50, hooks=[
-        dict(type='TextLoggerHook'),
-    ])
+# runtime settings
 total_epochs = 4
-dist_params = dict(backend='nccl')
-log_level = 'INFO'
-load_from = 'ckpts/cfaster_rcnn_r50_caffe_fpn_person_ap551.pth'
-resume_from = None
-workflow = [('train', 1)]
 evaluation = dict(metric=['bbox', 'track'], interval=1)
-work_dir = 'work_dirs/mot17'
--- configs/mot/qdtrack/qdtrack_faster_rcnn_r101_fpn_4e_mot17_crowdhuman.py
+++ configs/mot/qdtrack/qdtrack_faster_rcnn_r101_fpn_4e_mot17_crowdhuman.py
@@ -1,14 +1,7 @@
-_base_ = './qdtrack_faster_rcnn_r50_fpn.py'
-
-model = dict(
-    detector=dict(
-        backbone=dict(norm_cfg=dict(requires_grad=False), style='caffe'),
-        rpn_head=dict(bbox_coder=dict(clip_border=False)),
-        roi_head=dict(
-            bbox_head=dict(bbox_coder=dict(
-                clip_border=False), num_classes=1))),
-    track_head=dict(train_cfg=dict(assigner=dict(neg_iou_thr=0.5))))
-
+_base_ = [
+    './_base_/models/faster_rcnn_r50_fpn.py',
+    '../../_base_/datasets/mot_challenge.py', '../../_base_/default_runtime.py'
+]
 img_norm_cfg = dict(
     mean=[103.530, 116.280, 123.675], std=[1.0, 1.0, 1.0], to_rgb=False)
 train_pipeline = [
@@ -33,30 +26,16 @@ train_pipeline = [
     dict(type='MatchInstances', skip_nomatch=True),
     dict(
         type='VideoCollect',
-        keys=['img', 'gt_bboxes', 'gt_labels', 'gt_match_indices'],
-    ),
+        keys=[
+            'img', 'gt_bboxes', 'gt_labels', 'gt_match_indices',
+            'gt_instance_ids'
+        ]),
     dict(type='SeqDefaultFormatBundle', ref_prefix='ref')
-]
-test_pipeline = [
-    dict(type='LoadImageFromFile'),
-    dict(
-        type='MultiScaleFlipAug',
-        img_scale=(1088, 1088),
-        flip=False,
-        transforms=[
-            dict(type='Resize', keep_ratio=True),
-            dict(type='RandomFlip'),
-            dict(type='Normalize', **img_norm_cfg),
-            dict(type='Pad', size_divisor=32),
-            dict(type='ImageToTensor', keys=['img']),
-            dict(type='VideoCollect', keys=['img'])
-        ])
 ]
 mot_cfg = dict(
     type='MOTChallengeDataset',
     classes=('pedestrian', ),
     visibility_thr=-1,
-    # track_visibility_thr=-1,
     ann_file='data/MOT17/annotations/half-train_cocoformat.json',
     img_prefix='data/MOT17/train',
     ref_img_sampler=dict(num_ref_imgs=1, frame_range=10, method='uniform'),
@@ -68,44 +47,9 @@ crowdhuman_cfg = dict(
     ann_file='data/crowdhuman/annotations/crowdhuman_train.json',
     img_prefix='data/crowdhuman/train',
     pipeline=train_pipeline)
-
-dataset_type = 'MOTChallengeDataset'
-data_root = 'data/MOT17/'
 data = dict(
-    samples_per_gpu=2,
-    workers_per_gpu=2,
     train=dict(
+        _delete_=True,
         type='ConcatDataset',
         datasets=[mot_cfg, crowdhuman_cfg],
-        saparate_eval=False),
-    val=dict(
-        type=dataset_type,
-        ann_file='data/MOT17/annotations/half-val_cocoformat.json',
-        classes=['pedestrian'],
-        img_prefix='data/MOT17/train/',
-        ref_img_sampler=None,
-        pipeline=test_pipeline),
-    test=dict(
-        type=dataset_type,
-        ann_file='data/MOT17/annotations/half-val_cocoformat.json',
-        img_prefix='data/MOT17/train/',
-        classes=['pedestrian'],
-        ref_img_sampler=None,
-        pipeline=test_pipeline))
-
-optimizer = dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.0001)
-optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
-lr_config = dict(policy='step', step=[3])
-checkpoint_config = dict(interval=1)
-log_config = dict(
-    interval=50, hooks=[
-        dict(type='TextLoggerHook'),
-    ])
-total_epochs = 4
-dist_params = dict(backend='nccl')
-log_level = 'INFO'
-load_from = 'ckpts/converted_faster_rcnn_r50_caffe_fpn_person_ap551.pth'
-resume_from = None
-workflow = [('train', 1)]
-evaluation = dict(metric=['bbox', 'track'], interval=1)
-work_dir = 'work_dirs/mix'
+        saparate_eval=False))
