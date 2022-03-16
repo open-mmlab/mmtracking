fe63f366a563dc53ee1fa6164e5c89c72337095b
Tao Gong <gt950513@mail.ustc.edu.cn>
2022-03-16 16:33:08 +0800

    Update tools/dist_test.sh

--- tools/dist_test.sh
+++ tools/dist_test.sh
@@ -8,7 +8,7 @@ PORT=${PORT:-29500}
 MASTER_ADDR=${MASTER_ADDR:-"127.0.0.1"}
 
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
-python -m torch.distributed.launch 、
+python -m torch.distributed.launch \
     --nnodes=$NNODES \
     --node_rank=$NODE_RANK \
     --master_addr=$MASTER_ADDR \
