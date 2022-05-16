aedfa6ea76bad7771ecfe40ab05b403060a7cef1
lipengxiang.vendor <lipengxiang@pjlab.org.cn>
2022-05-16 10:37:47 +0000

    [Feature] Support YouTube VIS dataset

--- mmtrack/datasets/__init__.py
+++ mmtrack/datasets/__init__.py
@@ -4,8 +4,9 @@ from .base_video_dataset import BaseVideoDataset
 from .lasot_dataset import LaSOTDataset
 from .mot_challenge_dataset import MOTChallengeDataset
 from .reid_dataset import ReIDDataset
+from .youtube_vis_dataset import YouTubeVISDataset
 
 __all__ = [
     'BaseVideoDataset', 'MOTChallengeDataset', 'BaseSOTDataset',
-    'LaSOTDataset', 'ReIDDataset'
+    'LaSOTDataset', 'ReIDDataset', 'YouTubeVISDataset'
 ]
---
+++ tests/test_datasets/test_youtube_vis_dataset.py
@@ -0,0 +1,25 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+import os.path as osp
+from unittest import TestCase
+
+from mmtrack.datasets import YouTubeVISDataset
+
+PREFIX = osp.join(osp.dirname(__file__), '../data')
+# This is a demo annotation file for YouTubeVISDataset
+# 1 video, 1 categories ('sedan')
+# 1 images, 1 instances
+# 0 crowd
+DEMO_ANN_FILE = f'{PREFIX}/demo_vis_data/ann.json'
+
+
+class TestYouTubeVISDataset(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+
+        cls.dataset = YouTubeVISDataset(
+            ann_file=DEMO_ANN_FILE, dataset_version='2019')
+
+    def test_set_dataset_classes(self):
+        assert isinstance(self.dataset.metainfo, dict)
+        assert len(self.dataset.metainfo['CLASSES']) == 40
