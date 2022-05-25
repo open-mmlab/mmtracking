5dd24494d6d6b36c9a64b4ee958eeaa28b587557
JingweiZhang12 <zjw18@mails.tsinghua.edu.cn>
2022-05-25 13:32:51 +0800

    fix the bug of configs of Stark on lasot

--- configs/sot/stark/stark_st1_r50_500e_lasot.py
+++ configs/sot/stark/stark_st1_r50_500e_lasot.py
@@ -83,4 +83,8 @@ data = dict(
                 pipeline=train_pipeline,
                 split='train',
                 test_mode=False)
-        ]))
+        ]),
+    test=dict(
+        type='LaSOTDataset',
+        ann_file=data_root + 'lasot/annotations/lasot_test_infos.txt',
+        img_prefix=data_root + 'lasot/LaSOTBenchmark'))
