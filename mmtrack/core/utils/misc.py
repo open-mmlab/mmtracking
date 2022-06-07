9c907bab84e2a0e25005c7317b18fff16fd0e27d
Philipp Allgeuer <philipp.allgeuer@uni-hamburg.de>
2022-06-07 11:51:25 +0200

    Add torch_meshgrid_ij wrapper

--- mmtrack/core/utils/misc.py
+++ mmtrack/core/utils/misc.py
@@ -5,6 +5,8 @@ import platform
 import warnings
 
 import cv2
+import torch
+from packaging import version
 
 
 def setup_multi_processes(cfg):
@@ -37,3 +39,14 @@ def setup_multi_processes(cfg):
             f'overloaded, please further tune the variable for optimal '
             f'performance in your application as needed.')
         os.environ['MKL_NUM_THREADS'] = str(mkl_num_threads)
+
+
+_torch_version_meshgrid_indexing = version.parse(
+    torch.__version__) >= version.parse('1.10.0a0')
+
+
+def torch_meshgrid_ij(*tensors):
+    if _torch_version_meshgrid_indexing:
+        return torch.meshgrid(*tensors, indexing='ij')
+    else:
+        return torch.meshgrid(*tensors)  # Uses indexing='ij' by default
