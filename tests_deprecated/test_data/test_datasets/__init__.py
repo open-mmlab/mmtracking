50643f3044ee4c7bcabe2f8393341f234168cd2d
Tao Gong <gt950513@mail.ustc.edu.cn>
2022-06-28 16:59:25 +0800

    remove deprecated unittest

--- tests_deprecated/test_data/test_datasets/__init__.py
+++
@@ -1,4 +0,0 @@
-# Copyright (c) OpenMMLab. All rights reserved.
-from .utils import _create_coco_gt_results
-
-__all__ = ['_create_coco_gt_results']
--- tests_deprecated/test_models/test_forward/__init__.py
+++
@@ -1,4 +0,0 @@
-# Copyright (c) OpenMMLab. All rights reserved.
-from .utils import _demo_mm_inputs, _get_config_module
-
-__all__ = ['_demo_mm_inputs', '_get_config_module']
