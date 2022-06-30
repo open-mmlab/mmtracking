Fix bug of SOT and align the precision of siamrpn

mmtrack/core/evaluation/eval_sot_vot.py
@@ -13,7 +13,7 @@ except ImportError:
     vot = None
 
 
-def bbox2region(bbox: np.ndarray) -> Union[Rectangle, Polygon]:
+def bbox2region(bbox: np.ndarray) -> 'Union[Rectangle, Polygon]':
     """Convert bbox to Rectangle or Polygon Class object.
 
     Args:
@@ -138,7 +138,7 @@ def calc_accuracy(gt_trajectory: List[List],
                 mask[j] = False
         elif is_special(region, Special.FAILURE):
             mask[i] = False
-    return np.mean(overlaps[mask]) if any(mask) else 0.
+    return np.mean(overlaps[mask]).item() if any(mask) else 0.
 
 
 def eval_sot_accuracy_robustness(
@@ -309,6 +309,6 @@ def eval_sot_eao(
             all_successes.append(True)
 
     eao_curve = calc_eao_curve(all_overlaps, all_successes)
-    eao_score = np.mean(eao_curve[interval[0]:interval[1] + 1])
+    eao_score = np.mean(eao_curve[interval[0]:interval[1] + 1]).item()
     eao = dict(eao=eao_score)
     return eao

mmtrack/datasets/transforms/loading.py
@@ -123,9 +123,20 @@ class LoadTrackAnnotations(MMDet_LoadAnnotations):
             # ``gt_ignore_flags`` the same
             gt_ignore_flags = [False] * len(gt_bboxes)
 
-        results['gt_bboxes'] = np.array(
-            gt_bboxes, dtype=np.float32).reshape(-1, 4)
+        if len(gt_bboxes) > 0 and gt_bboxes[0].shape[0] == 8:
+            # The bbox of VOT2018 has (N, 8) shape and it's not possible to be
+            # empty.
+            results['gt_bboxes'] = np.array(
+                gt_bboxes, dtype=np.float32).reshape(-1, 8)
+        else:
+            # Some tasks, such as VID, may have empty bboxes and their bboxes
+            # need to be reshaped to (0, 4) format forcely in order to be
+            # compatible with ``TransformBroadcaster``.
+            results['gt_bboxes'] = np.array(
+                gt_bboxes, dtype=np.float32).reshape(-1, 4)
+
         if self.denorm_bbox:
+            assert results['gt_bboxes'].shape[1] == 4
             bbox_num = results['gt_bboxes'].shape[0]
             if bbox_num != 0:
                 assert 'width' in results and 'height' in results