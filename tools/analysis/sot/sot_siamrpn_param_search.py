f78913ff91c8c89981640da36ddfcb7f743690e6
zhangjingwei <zhangjingwei@sensetime.com>
2022-07-05 02:10:02 +0000

    Refactor parameter search of siamrpn++

--- tools/analysis/sot/sot_siamrpn_param_search.py
+++ tools/analysis/sot/sot_siamrpn_param_search.py
@@ -1,15 +1,15 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 import argparse
 import os
+import os.path as osp
 
 import numpy as np
-import torch
-from mmcv import Config, DictAction, get_logger, print_log
-from mmcv.cnn import fuse_conv_bn
-from mmcv.parallel import MMDataParallel, MMDistributedDataParallel
-from mmcv.runner import (get_dist_info, init_dist, load_checkpoint,
-                         wrap_fp16_model)
-from mmdet.datasets import build_dataset
+from mmengine.config import Config, DictAction
+from mmengine.dist import get_dist_info
+from mmengine.logging import print_log
+from mmengine.runner import Runner
+
+from mmtrack.utils import register_all_modules
 
 
 def parse_range(range_str):
@@ -21,9 +21,13 @@ def parse_range(range_str):
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(description='mmtrack test model')
+    parser = argparse.ArgumentParser(
+        description='MMTrack test (and eval) a model')
     parser.add_argument('config', help='test config file path')
     parser.add_argument('--checkpoint', help='checkpoint file')
+    parser.add_argument(
+        '--work-dir',
+        help='the directory to save the file containing evaluation metrics')
     parser.add_argument(
         '--penalty-k-range',
         type=parse_range,
@@ -40,40 +44,15 @@ def parse_args():
         help="the range of hyper-parameter 'window_influence' in SiamRPN++; the \
             format is 'start,stop,step'")
     parser.add_argument(
-        '--fuse-conv-bn',
-        action='store_true',
-        help='Whether to fuse conv and bn, this will slightly increase'
-        'the inference speed')
-    parser.add_argument('--log', help='log file', default=None)
-    parser.add_argument('--eval', type=str, nargs='+', help='eval types')
-    parser.add_argument('--show', action='store_true', help='show results')
-    parser.add_argument(
-        '--show-score-thr',
-        type=float,
-        default=0.3,
-        help='score threshold (default: 0.3)')
-    parser.add_argument(
-        '--show-dir', help='directory where painted images will be saved')
-    parser.add_argument(
-        '--gpu-collect',
-        action='store_true',
-        help='whether to use gpu to collect results.')
-    parser.add_argument(
-        '--tmpdir',
-        help='tmp directory used for collecting results from multiple '
-        'workers, available when gpu-collect is not specified')
-    parser.add_argument(
         '--cfg-options',
         nargs='+',
         action=DictAction,
         help='override some settings in the used config, the key-value pair '
-        'in xxx=yyy format will be merged into config file.')
-    parser.add_argument(
-        '--eval-options',
-        nargs='+',
-        action=DictAction,
-        help='custom options for evaluation, the key-value pair in xxx=yyy '
-        'format will be kwargs for dataset.evaluate() function')
+        'in xxx=yyy format will be merged into config file. If the value to '
+        'be overwritten is a list, it should be like key="[a,b]" or key=a,b '
+        'It also allows nested list/tuple values, e.g. key="[(a,b),(c,d)]" '
+        'Note that the quotation marks are necessary and that no white space '
+        'is allowed.')
     parser.add_argument(
         '--launcher',
         choices=['none', 'pytorch', 'slurm', 'mpi'],
@@ -86,94 +65,35 @@ def parse_args():
     return args
 
 
-def main():
-    args = parse_args()
+def parameter_search(runner, args):
+    cfg = runner.cfg
+    logger = runner.logger
 
-    assert args.eval or args.show \
-        or args.show_dir, \
-        ('Please specify at least one operation (eval/show the '
-         'results) with the argument "--eval"'
-         ', "--show" or "--show-dir"')
+    # calculate the number of all search cases and set comparing standard.
+    num_cases = len(args.penalty_k_range) * len(args.lr_range) * len(
+        args.win_influ_range)
+    case_count = 0
+    # compare function setting in parameter search. Now, the default comparing
+    # ruler is  `greater` because the model doesn't record comparing ruler
+    # of metrics in ``MMEngine``.
+    rule_map = {'greater': lambda x, y: x > y, 'less': lambda x, y: x < y}
+    compare_func = rule_map['greater']
 
-    cfg = Config.fromfile(args.config)
-
-    if cfg.get('USE_MMDET', False):
-        from mmdet.apis import multi_gpu_test, single_gpu_test
-        from mmdet.datasets import build_dataloader
-        from mmdet.models import build_detector as build_model
-        if 'detector' in cfg.model:
-            cfg.model = cfg.model.detector
-    elif cfg.get('USE_MMCLS', False):
-        from mmtrack.apis import multi_gpu_test, single_gpu_test
-        from mmtrack.datasets import build_dataloader
-        from mmtrack.models import build_reid as build_model
-        if 'reid' in cfg.model:
-            cfg.model = cfg.model.reid
+    if cfg.test_evaluator.metric == 'OPE':
+        eval_metrics = ['success', 'norm_precision', 'precision']
+        key_metric = 'success'
     else:
-        from mmtrack.apis import multi_gpu_test, single_gpu_test
-        from mmtrack.datasets import build_dataloader
-        from mmtrack.models import build_model
-    if args.cfg_options is not None:
-        cfg.merge_from_dict(args.cfg_options)
-    # set cudnn_benchmark
-    if cfg.get('cudnn_benchmark', False):
-        torch.backends.cudnn.benchmark = True
-    cfg.data.test.test_mode = True
+        eval_metrics = ['eao', 'accuracy', 'robustness', 'num_fails']
+        key_metric = 'eao'
 
-    # init distributed env first, since logger depends on the dist info.
-    if args.launcher == 'none':
-        distributed = False
-    else:
-        distributed = True
-        init_dist(args.launcher, **cfg.dist_params)
-
-    # build the dataloader
-    dataset = build_dataset(cfg.data.test)
-    data_loader = build_dataloader(
-        dataset,
-        samples_per_gpu=1,
-        workers_per_gpu=cfg.data.workers_per_gpu,
-        dist=distributed,
-        shuffle=False)
-
-    logger = get_logger('SOTParamsSearcher', log_file=args.log)
-
-    # build the model and load checkpoint
-    if cfg.get('test_cfg', False):
-        model = build_model(
-            cfg.model, train_cfg=cfg.train_cfg, test_cfg=cfg.test_cfg)
-    else:
-        model = build_model(cfg.model)
-    fp16_cfg = cfg.get('fp16', None)
-    if fp16_cfg is not None:
-        wrap_fp16_model(model)
-    if args.checkpoint is not None:
-        checkpoint = load_checkpoint(
-            model, args.checkpoint, map_location='cpu')
-        if 'meta' in checkpoint and 'CLASSES' in checkpoint['meta']:
-            model.CLASSES = checkpoint['meta']['CLASSES']
-    if not hasattr(model, 'CLASSES'):
-        model.CLASSES = dataset.CLASSES
-
-    if args.fuse_conv_bn:
-        model = fuse_conv_bn(model)
-
-    if not distributed:
-        model = MMDataParallel(model, device_ids=[0])
-    else:
-        model = MMDistributedDataParallel(
-            model.cuda(),
-            device_ids=[torch.cuda.current_device()],
-            broadcast_buffers=False)
+    checkpoint = runner.load_checkpoint(args.checkpoint)
 
     # init best_score, best_results and best parames
     if 'meta' in checkpoint and 'hook_msgs' in checkpoint[
-            'meta'] and 'best_score' in checkpoint['meta']['hook_msgs']:
-        best_score = checkpoint['meta']['hook_msgs']['best_score']
+            'meta'] and key_metric in checkpoint['meta']['hook_msgs']:
+        best_score = checkpoint['meta']['hook_msgs'][key_metric]
     else:
         best_score = 0
-
-    key_metric = cfg.evaluation.save_best
     best_result = {f'{key_metric}': best_score}
 
     best_params = dict(
@@ -182,14 +102,6 @@ def main():
         win_influ=cfg.model.test_cfg.rpn.window_influence)
     print_log(f'init best score as: {best_score}', logger)
     print_log(f'init best params as: {best_params}', logger)
-
-    num_cases = len(args.penalty_k_range) * len(args.lr_range) * len(
-        args.win_influ_range)
-    case_count = 0
-
-    # compare function setting in parameter search
-    rule_map = {'greater': lambda x, y: x > y, 'less': lambda x, y: x < y}
-    compare_func = rule_map[cfg.evaluation.rule]
 
     for penalty_k in args.penalty_k_range:
         for lr in args.lr_range:
@@ -204,54 +116,67 @@ def main():
                     f'penalty_k={penalty_k} lr={lr} win_influence={win_influ}',
                     logger)
 
-                if not distributed:
-                    outputs = single_gpu_test(
-                        model,
-                        data_loader,
-                        args.show,
-                        args.show_dir,
-                        show_score_thr=args.show_score_thr)
-                else:
-                    outputs = multi_gpu_test(model, data_loader, args.tmpdir,
-                                             args.gpu_collect)
+                # start testing
+                runner.test()
 
+                # parse the eluation results
+                res = dict()
+                for metric in eval_metrics:
+                    res[metric] = runner.message_hub.get_scalar(
+                        'test/sot/' + metric).current()
+
+                # show results
                 rank, _ = get_dist_info()
                 if rank == 0:
-                    kwargs = args.eval_options if args.eval_options else {}
-                    if args.eval:
-                        eval_kwargs = cfg.get('evaluation', {}).copy()
-                        # hard-code way to remove EvalHook args
-                        eval_hook_args = [
-                            'interval', 'tmpdir', 'start', 'gpu_collect',
-                            'save_best', 'rule', 'by_epoch'
-                        ]
-                        for key in eval_hook_args:
-                            eval_kwargs.pop(key, None)
-                        eval_kwargs.update(dict(metric=args.eval, **kwargs))
-                        eval_results = dataset.evaluate(outputs, **eval_kwargs)
-                        print_log(f'evaluation results: {eval_results}',
-                                  logger)
-                        print_log('------------------------------------------',
-                                  logger)
-
-                        if compare_func(eval_results[key_metric],
-                                        best_result[key_metric]):
-                            best_result = eval_results
-                            best_params['penalty_k'] = penalty_k,
-                            best_params['lr'] = lr,
-                            best_params['win_influ'] = win_influ
-
-                        print_log(
-                            f'The current best evaluation results: \
-                                {best_result}', logger)
-                        print_log(f'The current best params: {best_params}',
-                                  logger)
+                    print_log(f'evaluation results: {res}', logger)
+                    print_log('------------------------------------------',
+                              logger)
+                    if compare_func(res[key_metric], best_result[key_metric]):
+                        best_result = res
+                        best_params['penalty_k'] = penalty_k
+                        best_params['lr'] = lr
+                        best_params['win_influ'] = win_influ
+                    print_log(
+                        f'The current best evaluation results: {best_result}',
+                        logger)
+                    print_log(f'The current best params: {best_params}',
+                              logger)
 
     print_log(
-        f'After parameter searching, the best evaluation results: \
-            {best_result}', logger)
+        'After parameter searching, the best evaluation results: '
+        f'{best_result}', logger)
     print_log(f'After parameter searching, the best params: {best_params}',
               logger)
+
+
+def main():
+    args = parse_args()
+
+    # register all modules in mmtrack into the registries
+    # do not init the default scope here because it will be init in the runner
+    register_all_modules(init_default_scope=False)
+
+    # load config
+    cfg = Config.fromfile(args.config)
+    cfg.launcher = args.launcher
+    if args.cfg_options is not None:
+        cfg.merge_from_dict(args.cfg_options)
+
+    # work_dir is determined in this priority: CLI > segment in file > filename
+    if args.work_dir is not None:
+        # update configs according to CLI args if args.work_dir is not None
+        cfg.work_dir = args.work_dir
+    elif cfg.get('work_dir', None) is None:
+        # use config filename as default work_dir if cfg.work_dir is None
+        cfg.work_dir = osp.join('./work_dirs',
+                                osp.splitext(osp.basename(args.config))[0])
+
+    cfg.load_from = args.checkpoint
+
+    # build the runner from config
+    runner = Runner.from_cfg(cfg)
+
+    parameter_search(runner, args)
 
 
 if __name__ == '__main__':
