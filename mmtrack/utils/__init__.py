073325ef9bb50fe7cff1cb3004371d6c10c2b644
zhangjingwei <zhangjingwei@sensetime.com>
2022-07-11 11:19:42 +0000

    Refactor benchmark

--- mmtrack/utils/__init__.py
+++ mmtrack/utils/__init__.py
@@ -1,5 +1,10 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+from .benchmark import (DataLoaderBenchmark, DatasetBenchmark,
+                        InferenceBenchmark)
 from .collect_env import collect_env
 from .setup_env import register_all_modules
 
-__all__ = ['collect_env', 'register_all_modules']
+__all__ = [
+    'collect_env', 'register_all_modules', 'DataLoaderBenchmark',
+    'DatasetBenchmark', 'InferenceBenchmark'
+]
