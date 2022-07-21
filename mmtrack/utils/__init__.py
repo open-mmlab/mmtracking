643c9d4f0fa3cc3df3c9ddcdc1a1106c812971c3
zhangjingwei <zhangjingwei@sensetime.com>
2022-07-21 03:12:58 +0000

    Rename visualization.py to mot_error_visualization.py

--- mmtrack/utils/__init__.py
+++ mmtrack/utils/__init__.py
@@ -4,11 +4,11 @@ from .benchmark import (DataLoaderBenchmark, DatasetBenchmark,
 from .collect_env import collect_env
 from .image import crop_image, imrenormalize
 from .misc import convert_data_sample_type, stack_batch
+from .mot_error_visualization import imshow_mot_errors
 from .setup_env import register_all_modules
 from .typing import (ConfigType, ForwardResults, InstanceList, MultiConfig,
                      OptConfigType, OptInstanceList, OptMultiConfig,
                      OptSampleList, SampleList)
-from .visualization import imshow_mot_errors
 
 __all__ = [
     'collect_env', 'register_all_modules', 'DataLoaderBenchmark',
