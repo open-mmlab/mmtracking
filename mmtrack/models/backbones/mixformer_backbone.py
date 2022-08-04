1bad0c071c99a1e37fe16615d7e49a5d2d1f1d60
songtianhui <songtianhui@megvii.com>
2022-08-05 03:28:34 +0800

    replace torch._six.container_abcs with collections

--- mmtrack/models/backbones/mixformer_backbone.py
+++ mmtrack/models/backbones/mixformer_backbone.py
@@ -1,4 +1,5 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+import collections.abc as container_abcs
 import logging
 from collections import OrderedDict
 from functools import partial
@@ -14,13 +15,6 @@ from mmdet.models.builder import BACKBONES
 from timm.models.layers import DropPath, trunc_normal_
 
 from .utils import FrozenBatchNorm2d
-
-TORCH_MAJOR = int(torch.__version__.split('.')[0])
-TORCH_MINOR = int(torch.__version__.split('.')[1])
-if TORCH_MAJOR == 1 and TORCH_MINOR < 8:
-    from torch._six import container_abcs
-else:
-    import collections.abc as container_abcs
 
 
 # From PyTorch internals
