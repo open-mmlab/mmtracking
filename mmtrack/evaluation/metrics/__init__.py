ede02685fdaf578eb95989e2b763a29b94cc507e
Pengxiang Li <lipengxiang@pjlab.org.cn>
2022-08-05 14:23:38 +0800

    Testing & Training Alignment of QDTrack on TAO dataset (#629)
    * init commit
    
    * update lvis cfg
    
    * update cfg
    
    * fix train
    
    * fix cfg resize
    
    * fix rpn loss
    
    * fix tao.py
    
    * init commit
    
    * update lvis cfg
    
    * update cfg
    
    * fix train
    
    * fix cfg resize
    
    * fix rpn loss
    
    * fix tao.py
    
    * revert init
    
    * del self._tao_meta_info
    
    * update some code
    
    * del bgr2rgb
    
    * update

--- mmtrack/evaluation/metrics/__init__.py
+++ mmtrack/evaluation/metrics/__init__.py
@@ -4,8 +4,8 @@ from .coco_video_metric import CocoVideoMetric
 from .mot_challenge_metrics import MOTChallengeMetrics
 from .reid_metrics import ReIDMetrics
 from .sot_metrics import SOTMetric
-from .tao_metric import TAOMetric
-from .youtube_vis_metric import YouTubeVISMetric
+from .tao_metrics import TAOMetric
+from .youtube_vis_metrics import YouTubeVISMetric
 
 __all__ = [
     'ReIDMetrics', 'BaseVideoMetric', 'CocoVideoMetric', 'YouTubeVISMetric',
--- mmtrack/evaluation/metrics/tao_metrics.py
+++ mmtrack/evaluation/metrics/tao_metrics.py
@@ -17,6 +17,12 @@ try:
 except ImportError:
     tao = None
 
+try:
+    import lvis
+    from lvis import LVIS, LVISEval, LVISResults
+except ImportError:
+    lvis = None
+
 
 @METRICS.register_module()
 class TAOMetric(BaseVideoMetric):
@@ -54,18 +60,16 @@ class TAOMetric(BaseVideoMetric):
         # tao evaluation metrics
         self.metrics = metric if isinstance(metric, list) else [metric]
         self.format_only = format_only
-        allowed_metrics = ['tao_track_ap']
+        allowed_metrics = ['tao_track_ap', 'bbox']
         for metric in self.metrics:
             if metric not in allowed_metrics:
-                raise KeyError(
-                    f"metric should be 'tao_track_ap', but got {metric}.")
+                raise KeyError(f"metric should be 'tao_track_ap' or 'bbox',"
+                               f' but got {metric}.')
 
         self.metric_items = metric_items
         self.outfile_prefix = outfile_prefix
         self.per_video_res = []
         self.img_ids = []
-        self.cat_ids = []
-        self._tao_meta_info = defaultdict(list)
 
     def process(self, data_batch: Sequence[dict],
                 predictions: Sequence[dict]) -> None:
@@ -81,15 +85,21 @@ class TAOMetric(BaseVideoMetric):
         """
         for data, pred in zip(data_batch, predictions):
             result = dict()
-            pred = pred['pred_track_instances']
+            pred_track = pred['pred_track_instances']
+            pred_det = pred['pred_det_instances']
             frame_id = data['data_sample']['frame_id']
             video_length = data['data_sample']['video_length']
 
             result['img_id'] = data['data_sample']['img_id']
-            result['bboxes'] = pred['bboxes'].cpu().numpy()
-            result['scores'] = pred['scores'].cpu().numpy()
-            result['labels'] = pred['labels'].cpu().numpy()
-            result['instances_id'] = pred['instances_id'].cpu().numpy()
+            result['track_bboxes'] = pred_track['bboxes'].cpu().numpy()
+            result['track_scores'] = pred_track['scores'].cpu().numpy()
+            result['track_labels'] = pred_track['labels'].cpu().numpy()
+            result['track_instances_id'] = pred_track['instances_id'].cpu(
+            ).numpy()
+
+            result['det_bboxes'] = pred_det['bboxes'].cpu().numpy()
+            result['det_scores'] = pred_det['scores'].cpu().numpy()
+            result['det_labels'] = pred_det['labels'].cpu().numpy()
 
             # parse gt
             gt = dict()
@@ -119,11 +129,13 @@ class TAOMetric(BaseVideoMetric):
             if frame_id == video_length - 1:
                 preds, gts = zip(*self.per_video_res)
                 # format the results
-                gt_results = self._format_one_video_gts(gts)
-                pred_results = self._format_one_video_preds(preds)
+                gt_results, tao_meta_info = self._format_one_video_gts(gts)
+                pred_track_results, pred_det_results = \
+                    self._format_one_video_preds(preds, tao_meta_info)
                 self.per_video_res.clear()
                 # add converted result to the results list
-                self.results.append((pred_results, gt_results))
+                self.results.append((pred_track_results, pred_det_results,
+                                     gt_results, tao_meta_info))
 
     def compute_metrics(self, results: List) -> Dict[str, float]:
         """Compute the metrics from processed results.
@@ -137,12 +149,25 @@ class TAOMetric(BaseVideoMetric):
         """
         logger: MMLogger = MMLogger.get_current_instance()
         # split gt and prediction list
-        tmp_pred_results, tmp_gt_results = zip(*results)
-        gt_results = self.format_gts(tmp_gt_results)
-        pred_results = self.format_preds(tmp_pred_results)
+        tmp_pred_track_results, tmp_pred_det_results, \
+            tmp_gt_results, tmp_meta_info = zip(*results)
+        tao_meta_info = self.format_meta(tmp_meta_info)
+        gt_results = self.format_gts(tmp_gt_results, tao_meta_info)
+        pred_track_results = self.format_preds(tmp_pred_track_results)
+        pred_det_results = self.format_preds(tmp_pred_det_results)
+
+        if 'bbox' in self.metrics:
+            # LVIS api only supports reading from files, hence,
+            # save the json result to tmp dir
+            tmp_dir = tempfile.TemporaryDirectory()
+            pred_det_results_path = f'{tmp_dir.name}/tao_bbox.json'
+            gt_results_path = f'{tmp_dir.name}/tao_gt.json'
+            mmcv.dump(pred_det_results, pred_det_results_path)
+            mmcv.dump(gt_results, gt_results_path)
 
         if self.format_only:
-            self.save_pred_results(pred_results)
+            self.save_pred_results(pred_track_results, 'track')
+            self.save_pred_results(pred_det_results, 'det')
             return dict()
 
         eval_results = dict()
@@ -156,9 +181,10 @@ class TAOMetric(BaseVideoMetric):
 
             logger.info('Evaluating tracking results...')
             tao_gt = Tao(gt_results)
-            tao_eval = TaoEval(tao_gt, pred_results)
+            tao_eval = TaoEval(tao_gt, pred_track_results)
             tao_eval.params.img_ids = self.img_ids
-            tao_eval.params.cat_ids = self.cat_ids
+            tao_eval.params.cat_ids = list(
+                self.dataset_meta['categories'].keys())
             tao_eval.params.iou_thrs = np.array([0.5, 0.75])
             tao_eval.run()
 
@@ -170,23 +196,64 @@ class TAOMetric(BaseVideoMetric):
                     val = float('{:.3f}'.format(float(v)))
                     eval_results[key] = val
 
+        if 'bbox' in self.metrics:
+            if lvis is None:
+                raise ImportError(
+                    'Please run'
+                    ' pip install git+https://github.com/lvis-dataset/lvis-api.git '  # noqa
+                    'to manually install lvis')
+
+            logger.info('Evaluating detection results...')
+            lvis_gt = LVIS(gt_results_path)
+            lvis_dt = LVISResults(lvis_gt, pred_det_results_path)
+            lvis_eval = LVISEval(lvis_gt, lvis_dt, 'bbox')
+            lvis_eval.params.imgIds = self.img_ids
+            lvis_eval.params.catIds = list(
+                self.dataset_meta['categories'].keys())
+            lvis_eval.evaluate()
+            lvis_eval.accumulate()
+            lvis_eval.summarize()
+            lvis_eval.print_results()
+
+            lvis_results = lvis_eval.get_results()
+            for k, v in lvis_results.items():
+                if k.startswith('AP'):
+                    key = '{}_{}'.format('bbox', k)
+                    val = float('{:.3f}'.format(float(v)))
+                    eval_results[key] = val
+            tmp_dir.cleanup()
         return eval_results
 
-    def format_gts(self, gts: Tuple[List]) -> dict:
+    def format_meta(self, parts_meta: Tuple[dict]) -> dict:
+        """Gather all meta info from self.results."""
+        all_seq_vids_info = []
+        all_seq_imgs_info = []
+        all_seq_tracks_info = []
+        for _seq_info in parts_meta:
+            all_seq_vids_info.extend(_seq_info['videos'])
+            all_seq_imgs_info.extend(_seq_info['images'])
+            all_seq_tracks_info.extend(_seq_info['tracks'])
+
+        # update tao_meta_info
+        tao_meta_info = dict(
+            videos=all_seq_vids_info,
+            images=all_seq_imgs_info,
+            tracks=all_seq_tracks_info)
+
+        return tao_meta_info
+
+    def format_gts(self, gts: Tuple[List], tao_meta_info: dict) -> dict:
         """Gather all ground-truth from self.results."""
-        categories = []
-        for id, name in enumerate(self.dataset_meta['CLASSES']):
-            categories.append(dict(id=id + 1, name=name))
-            self.cat_ids.append(id + 1)
-        for img_info in self._tao_meta_info['images']:
+        categories = list(self.dataset_meta['categories'].values())
+        for img_info in tao_meta_info['images']:
             self.img_ids.append(img_info['id'])
         gt_results = dict(
             info=dict(),
-            images=self._tao_meta_info['images'],
+            images=tao_meta_info['images'],
             categories=categories,
-            videos=self._tao_meta_info['videos'],
+            videos=tao_meta_info['videos'],
             annotations=[],
-            tracks=self._tao_meta_info['tracks'])
+            tracks=tao_meta_info['tracks'])
 
         ann_id = 1
         for gt_result in gts:
@@ -200,34 +267,49 @@ class TAOMetric(BaseVideoMetric):
     def format_preds(self, preds: Tuple[List]) -> List:
         """Gather all predictions from self.results."""
         pred_results = []
+        max_track_id = 0
         for pred_result in preds:
+            # update track id
+            if 'track_id' in pred_result[0]:
+                track_ids = []
+                for ins_info in pred_result:
+                    track_ids.append(ins_info['track_id'])
+                    ins_info['track_id'] += max_track_id
+                track_ids = list(set(track_ids))
+                max_track_id += max(track_ids) + 1
+
             pred_results.extend(pred_result)
         return pred_results
 
-    def _format_one_video_preds(self, pred_dicts: Tuple[dict]) -> List:
+    def _format_one_video_preds(self, pred_dicts: Tuple[dict],
+                                tao_meta_info: Dict) -> Tuple[List, List]:
         """Convert the annotation to the format of YouTube-VIS.
 
         This operation is to make it easier to use the official eval API.
 
         Args:
             pred_dicts (Tuple[dict]): Prediction of the dataset.
+            tao_meta_info (dict): A dict containing videos and images
+                information of TAO.
 
         Returns:
             List: The formatted predictions.
         """
         # Collate preds scatters (tuple of dict to dict of list)
         preds = defaultdict(list)
+        cat_ids = list(self.dataset_meta['categories'].keys())
         for pred in pred_dicts:
             for key in pred.keys():
                 preds[key].append(pred[key])
 
-        vid_infos = self._tao_meta_info['videos']
-        json_results = []
+        vid_infos = tao_meta_info['videos']
+        track_json_results = []
+        det_json_results = []
         video_id = vid_infos[-1]['id']
 
         for img_id, bboxes, scores, labels, ins_ids in zip(
-                preds['img_id'], preds['bboxes'], preds['scores'],
-                preds['labels'], preds['instances_id']):
+                preds['img_id'], preds['track_bboxes'], preds['track_scores'],
+                preds['track_labels'], preds['track_instances_id']):
             for bbox, score, label, ins_id in zip(bboxes, scores, labels,
                                                   ins_ids):
                 data = dict(
@@ -239,14 +321,33 @@ class TAOMetric(BaseVideoMetric):
                         bbox[3] - bbox[1],
                     ],
                     score=score,
-                    category_id=label + 1,
+                    category_id=cat_ids[label],
                     track_id=ins_id,
                     video_id=video_id)
-                json_results.append(data)
+                track_json_results.append(data)
 
-        return json_results
+        for img_id, bboxes, scores, labels in zip(preds['img_id'],
+                                                  preds['det_bboxes'],
+                                                  preds['det_scores'],
+                                                  preds['det_labels']):
+            for bbox, score, label in zip(bboxes, scores, labels):
+                data = dict(
+                    image_id=img_id,
+                    bbox=[
+                        bbox[0],
+                        bbox[1],
+                        bbox[2] - bbox[0],
+                        bbox[3] - bbox[1],
+                    ],
+                    score=score,
+                    category_id=cat_ids[label],
+                    video_id=video_id)
+                det_json_results.append(data)
 
-    def _format_one_video_gts(self, gt_dicts: Tuple[dict]) -> List:
+        return track_json_results, det_json_results
+
+    def _format_one_video_gts(self,
+                              gt_dicts: Tuple[dict]) -> Tuple[list, dict]:
         """Convert the annotation to the format of YouTube-VIS.
 
         This operation is to make it easier to use the official eval API.
@@ -255,13 +356,16 @@ class TAOMetric(BaseVideoMetric):
             gt_dicts (Tuple[dict]): Ground truth of the dataset.
 
         Returns:
-            list: The formatted gts.
+            Tuple[list, dict]: The formatted gts and a dict containing videos
+            and images information of TAO.
         """
         video_infos = []
         image_infos = []
         track_infos = []
         annotations = []
         instance_flag = dict()  # flag the ins_id is used or not
+        tao_meta_info = defaultdict(list)
+        cat_ids = list(self.dataset_meta['categories'].keys())
 
         # get video infos
         for gt_dict in gt_dicts:
@@ -311,30 +415,31 @@ class TAOMetric(BaseVideoMetric):
                     track_id=instance_id,
                     instance_id=instance_id,
                     iscrowd=ann.get('ignore_flag', 0),
-                    category_id=label + 1,
+                    category_id=cat_ids[label],
                     area=coco_bbox[2] * coco_bbox[3])
                 if not instance_flag.get(instance_id, False):
                     track_info = dict(
                         id=instance_id,
-                        category_id=label + 1,
+                        category_id=cat_ids[label],
                         video_id=video_id)
                     track_infos.append(track_info)
                     instance_flag[instance_id] = True
                 annotations.append(annotation)
 
         # update tao meta info
-        self._tao_meta_info['images'].extend(image_infos)
-        self._tao_meta_info['videos'].extend(video_infos)
-        self._tao_meta_info['tracks'].extend(track_infos)
+        tao_meta_info['images'].extend(image_infos)
+        tao_meta_info['videos'].extend(video_infos)
+        tao_meta_info['tracks'].extend(track_infos)
 
-        return annotations
+        return annotations, tao_meta_info
 
-    def save_pred_results(self, pred_results: List) -> None:
+    def save_pred_results(self, pred_results: List, res_type: str) -> None:
         """Save the results to a zip file.
 
         Args:
             pred_results (list): Testing results of the
                 dataset.
+            res_type (str): The type of testing results, track or detection.
         """
         logger: MMLogger = MMLogger.get_current_instance()
         if self.outfile_prefix is None:
@@ -342,6 +447,6 @@ class TAOMetric(BaseVideoMetric):
             outfile_prefix = osp.join(tmp_dir.name, 'results')
         else:
             outfile_prefix = self.outfile_prefix
-        mmcv.dump(pred_results, f'{outfile_prefix}.json')
+        mmcv.dump(pred_results, f'{outfile_prefix}_{res_type}.json')
 
-        logger.info(f'save the results to {outfile_prefix}.json')
+        logger.info(f'save the results to {outfile_prefix}_{res_type}.json')
