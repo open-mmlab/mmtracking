37d6728c822774bb8e42fe14cf1c260f4e8d22f3
Jinkun Cao <jinkuncao@fb.com>
2022-08-15 00:05:09 -0400

    resolve conflict

--- mmtrack/models/trackers/__init__.py
+++ mmtrack/models/trackers/__init__.py
@@ -2,12 +2,13 @@
 from .base_tracker import BaseTracker
 from .byte_tracker import ByteTracker
 from .masktrack_rcnn_tracker import MaskTrackRCNNTracker
+from .quasi_dense_tao_tracker import QuasiDenseTAOTracker
+from .quasi_dense_tracker import QuasiDenseTracker
 from .ocsort_tracker import OCSORTTracker
-from .quasi_dense_embed_tracker import QuasiDenseEmbedTracker
 from .sort_tracker import SortTracker
 from .tracktor_tracker import TracktorTracker
 
 __all__ = [
     'BaseTracker', 'TracktorTracker', 'SortTracker', 'MaskTrackRCNNTracker',
-    'ByteTracker', 'QuasiDenseEmbedTracker', 'OCSORTTracker'
+    'ByteTracker', 'QuasiDenseTracker', 'QuasiDenseTAOTracker', 'OCSORTTracker'
 ]
