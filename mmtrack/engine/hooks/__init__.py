195048beefda8e274fe99467d33bbe92b29ba8c4
Jingwei Zhang <zjw18@mails.tsinghua.edu.cn>
2022-08-30 17:55:38 +0800

    [Enhance] Add SiamRPNBackboneUnfreezeHook (#697)
    * add SiamRPNBackboneUnfreezeHook
    
    * add ut for siamrpnunfreezehook
    
    * use is_model_wrapper
    
    * reduce channels of mot models
    
    * reduce channels of mot models

--- mmtrack/engine/hooks/__init__.py
+++ mmtrack/engine/hooks/__init__.py
@@ -1,5 +1,9 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+from .siamrpn_backbone_unfreeze_hook import SiamRPNBackboneUnfreezeHook
 from .visualization_hook import TrackVisualizationHook
 from .yolox_mode_switch_hook import YOLOXModeSwitchHook
 
-__all__ = ['YOLOXModeSwitchHook', 'TrackVisualizationHook']
+__all__ = [
+    'YOLOXModeSwitchHook', 'TrackVisualizationHook',
+    'SiamRPNBackboneUnfreezeHook'
+]
