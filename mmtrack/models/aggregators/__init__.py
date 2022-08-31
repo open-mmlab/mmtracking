b4d96b863af1f7f1e6979a0de99e8e363eb30a73
khurramHashmi <khurramhashim94@gmail.com>
2022-08-31 14:29:23 +0200

    [Feature] Incorporating TF-Blender for VOD

--- mmtrack/models/aggregators/__init__.py
+++ mmtrack/models/aggregators/__init__.py
@@ -1,5 +1,6 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .embed_aggregator import EmbedAggregator
 from .selsa_aggregator import SelsaAggregator
+from .tfblender_aggregator import TFBlenderAggregator
 
-__all__ = ['EmbedAggregator', 'SelsaAggregator']
+__all__ = ['EmbedAggregator', 'SelsaAggregator', 'TFBlenderAggregator']
