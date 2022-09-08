3d7d6ad7b504b10cacb36d3faa4c2ac2d3b6f81c
JingweiZhang12 <zjw18@mails.tsinghua.edu.cn>
2022-09-08 16:47:07 +0800

    fix options_after_eval

--- mmtrack/evaluation/metrics/sot_metrics.py
+++ mmtrack/evaluation/metrics/sot_metrics.py
@@ -45,8 +45,8 @@ class SOTMetric(BaseVideoMetric):
             metric names to disambiguate homonymous metrics of different
             evaluators. If prefix is not provided in the argument,
             self.default_prefix will be used instead. Defaults to None.
-        options_after_eval (Optional[dict], optional): The options after
-            evaluation. The usage is the following:
+        options_after_eval (dict, optional): The options after
+            evaluation. Defaults to {}. The usage is the following:
             ```
                 options_after_eval = dict(
                     saved_eval_res_file = './results/sot_res.json',
@@ -70,7 +70,7 @@ class SOTMetric(BaseVideoMetric):
                  outfile_prefix: Optional[str] = None,
                  collect_device: str = 'cpu',
                  prefix: Optional[str] = None,
-                 options_after_eval: Optional[dict] = None) -> None:
+                 options_after_eval: dict = {}) -> None:
         super().__init__(collect_device=collect_device, prefix=prefix)
         self.metrics = metric if isinstance(metric, list) else [metric]
         assert not (
