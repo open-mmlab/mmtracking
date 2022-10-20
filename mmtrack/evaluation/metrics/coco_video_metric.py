125fac19ec60d5df6b770625c716f03b5a4ec7f3
JingweiZhang12 <zjw18@mails.tsinghua.edu.cn>
2022-10-20 19:09:45 +0800

    fix some bugs

--- mmtrack/evaluation/metrics/coco_video_metric.py
+++ mmtrack/evaluation/metrics/coco_video_metric.py
@@ -1,19 +1,14 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-import warnings
-from typing import Optional, Sequence
+from typing import Sequence
 
-from mmdet.datasets.api_wrappers import COCO
+from mmdet.evaluation import CocoMetric
 from mmdet.structures.mask import encode_mask_results
-from mmengine.dist import broadcast_object_list, is_main_process
-from mmengine.fileio import FileClient
-from mmeval import CocoMetric as _CocoMetric
 
 from mmtrack.registry import METRICS
-from .base_video_metrics import collect_tracking_results
 
 
 @METRICS.register_module()
-class CocoVideoMetric(_CocoMetric):
+class CocoVideoMetric(CocoMetric):
     """COCO evaluation metric.
 
     Evaluate AR, AP, and mAP for detection tasks including proposal/box
@@ -22,18 +17,13 @@ class CocoVideoMetric(_CocoMetric):
     """
 
     def __init__(self,
-                 ann_file: Optional[str] = None,
                  dist_collect_mode='cat',
+                 dist_backend='torch_cuda',
                  **kwargs) -> None:
-        super().__init__(dist_collect_mode=dist_collect_mode, **kwargs)
-        # if ann_file is not specified,
-        # initialize coco api with the converted dataset
-        # if ann_file:
-        #     file_client = FileClient.infer_client(uri=ann_file)
-        #     with file_client.get_local_path(ann_file) as local_path:
-        #         self._coco_api = COCO(local_path)
-        # else:
-        #     self._coco_api = None
+        super().__init__(
+            dist_collect_mode=dist_collect_mode,
+            dist_backend=dist_backend,
+            **kwargs)
 
     def process(self, data_batch: dict, data_samples: Sequence[dict]) -> None:
         """Process one batch of data samples and predictions. The processed
@@ -50,74 +40,26 @@ class CocoVideoMetric(_CocoMetric):
         """
         predictions, groundtruths = [], []
         for data_sample in data_samples:
-            result = dict()
-            pred = data_sample['pred_det_instances']
-            result['img_id'] = data_sample['img_id']
-            result['bboxes'] = pred['bboxes'].cpu().numpy()
-            result['scores'] = pred['scores'].cpu().numpy()
-            result['labels'] = pred['labels'].cpu().numpy()
-            # encode mask to RLE
-            if 'masks' in pred:
-                result['masks'] = encode_mask_results(
-                    pred['masks'].detach().cpu().numpy())
+            pred = dict()
+            pred_instances = data_sample['pred_det_instances']
+            pred['img_id'] = data_sample['img_id']
+            pred['bboxes'] = pred_instances['bboxes'].cpu().numpy()
+            pred['scores'] = pred_instances['scores'].cpu().numpy()
+            pred['labels'] = pred_instances['labels'].cpu().numpy()
+            if 'masks' in pred_instances:
+                pred['masks'] = encode_mask_results(
+                    pred_instances['masks'].detach().cpu().numpy())
             # some detectors use different scores for bbox and mask
-            if 'mask_scores' in pred:
-                result['mask_scores'] = pred['mask_scores'].cpu().numpy()
+            if 'mask_scores' in pred_instances:
+                pred['mask_scores'] = \
+                    pred_instances['mask_scores'].cpu().numpy()
+            predictions.append(pred)
 
+            # parse gt
             if self._coco_api is None:
                 ann = self.add_gt(data_sample)
             else:
                 ann = dict()
             groundtruths.append(ann)
 
-            self.add(predictions, groundtruths)
-
-            # parse gt
-            # gt = dict()
-            # gt['width'] = data_sample['ori_shape'][1]
-            # gt['height'] = data_sample['ori_shape'][0]
-            # gt['img_id'] = data_sample['img_id']
-            # if self._coco_api is None:
-            #     assert 'instances' in data_sample, \
-            #         'ground truth is required for evaluation when ' \
-            #         '`ann_file` is not provided'
-            #     gt['anns'] = data_sample['instances']
-            # # add converted result to the results list
-            # self.results.append((gt, result))
-
-    # def evaluate(self, size: int) -> dict:
-    #     """Evaluate the model performance of the whole dataset after processing
-    #     all batches.
-
-    #     Args:
-    #         size (int): Length of the entire validation dataset.
-
-    #     Returns:
-    #         dict: Evaluation metrics dict on the val dataset. The keys are the
-    #         names of the metrics, and the values are corresponding results.
-    #     """
-    #     if len(self.results) == 0:
-    #         warnings.warn(
-    #             f'{self.__class__.__name__} got empty `self.results`. Please '
-    #             'ensure that the processed results are properly added into '
-    #             '`self.results` in `process` method.')
-
-    #     results = collect_tracking_results(self.results, self.collect_device)
-
-    #     if is_main_process():
-    #         _metrics = self.compute_metrics(results)  # type: ignore
-    #         # Add prefix to metric names
-    #         if self.prefix:
-    #             _metrics = {
-    #                 '/'.join((self.prefix, k)): v
-    #                 for k, v in _metrics.items()
-    #             }
-    #         metrics = [_metrics]
-    #     else:
-    #         metrics = [None]  # type: ignore
-
-    #     broadcast_object_list(metrics)
-
-    #     # reset the results list
-    #     self.results.clear()
-    #     return metrics[0]
+        self.add(predictions, groundtruths)
