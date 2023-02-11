9b2509a297ace02b0cebfad57ecb94bf859266b8
hoang1007 <hoangvuhuy1007@gmail.com>
2023-02-11 21:09:41 +0700

    mmtrack 1.x support mmyolo

---
+++ configs/_base_/models/yolov7_l_syncbn_fast.py
@@ -0,0 +1,101 @@
+# parameters that often need to be modified
+img_scale = (640, 640)  # width, height
+max_epochs = 300
+save_epoch_intervals = 10
+train_batch_size_per_gpu = 16
+train_num_workers = 8
+# persistent_workers must be False if num_workers is 0.
+persistent_workers = True
+val_batch_size_per_gpu = 1
+val_num_workers = 2
+
+# only on Val
+batch_shapes_cfg = dict(
+    type='BatchShapePolicy',
+    batch_size=val_batch_size_per_gpu,
+    img_size=img_scale[0],
+    size_divisor=32,
+    extra_pad_ratio=0.5)
+
+# different from yolov5
+anchors = [
+    [(12, 16), (19, 36), (40, 28)],  # P3/8
+    [(36, 75), (76, 55), (72, 146)],  # P4/16
+    [(142, 110), (192, 243), (459, 401)]  # P5/32
+]
+strides = [8, 16, 32]
+num_det_layers = 3
+num_classes = 80
+
+model = dict(
+    data_preprocessor=dict(
+        type='TrackDataPreprocessor',
+        mean=[0., 0., 0.],
+        std=[255., 255., 255.],
+        bgr_to_rgb=True,
+        rgb_to_bgr=False,
+        pad_size_divisor=32),
+    detector=dict(
+        type='YOLODetector',
+        _scope_='mmyolo',
+        backbone=dict(
+            type='YOLOv7Backbone',
+            arch='L',
+            norm_cfg=dict(type='BN', momentum=0.03, eps=0.001),
+            act_cfg=dict(type='SiLU', inplace=True)),
+        neck=dict(
+            type='YOLOv7PAFPN',
+            block_cfg=dict(
+                type='ELANBlock',
+                middle_ratio=0.5,
+                block_ratio=0.25,
+                num_blocks=4,
+                num_convs_in_block=1),
+            upsample_feats_cat_first=False,
+            in_channels=[512, 1024, 1024],
+            # The real output channel will be multiplied by 2
+            out_channels=[128, 256, 512],
+            norm_cfg=dict(type='BN', momentum=0.03, eps=0.001),
+            act_cfg=dict(type='SiLU', inplace=True)),
+        bbox_head=dict(
+            type='YOLOv7Head',
+            head_module=dict(
+                type='YOLOv7HeadModule',
+                num_classes=num_classes,
+                in_channels=[256, 512, 1024],
+                featmap_strides=strides,
+                num_base_priors=3),
+            prior_generator=dict(
+                type='mmdet.YOLOAnchorGenerator',
+                base_sizes=anchors,
+                strides=strides),
+            # scaled based on number of detection layers
+            loss_cls=dict(
+                type='mmdet.CrossEntropyLoss',
+                use_sigmoid=True,
+                reduction='mean',
+                loss_weight=0.3 * (num_classes / 80 * 3 / num_det_layers)),
+            loss_bbox=dict(
+                type='IoULoss',
+                iou_mode='ciou',
+                bbox_format='xywh',
+                reduction='mean',
+                loss_weight=0.05 * (3 / num_det_layers),
+                return_iou=True),
+            loss_obj=dict(
+                type='mmdet.CrossEntropyLoss',
+                use_sigmoid=True,
+                reduction='mean',
+                loss_weight=0.7 * ((img_scale[0] / 640)**2 * 3 / num_det_layers)),
+            obj_level_weights=[4., 1., 0.4],
+            # BatchYOLOv7Assigner params
+            prior_match_thr=4.,
+            simota_candidate_topk=10,
+            simota_iou_weight=3.0,
+            simota_cls_weight=1.0),
+        test_cfg=dict(
+            multi_label=True,
+            nms_pre=30000,
+            score_thr=0.001,
+            nms=dict(type='nms', iou_threshold=0.65),
+            max_per_img=300)))
---
+++ configs/mot/deepsort/my_config.py
@@ -0,0 +1,58 @@
+_base_ = [
+    '../../_base_/models/yolov7_l_syncbn_fast.py',
+    '../../_base_/datasets/mot_challenge.py', 
+    '../../_base_/default_runtime.py'
+]
+model = dict(
+    type='DeepSORT',
+    detector=dict(
+        init_cfg=dict(
+            type='Pretrained',
+            checkpoint=  # noqa: E251
+            'https://download.openmmlab.com/mmyolo/v0/yolov7/yolov7_l_syncbn_fast_8x16b-300e_coco/yolov7_l_syncbn_fast_8x16b-300e_coco_20221123_023601-8113c0eb.pth'  # noqa: E501
+        )),
+    motion=dict(type='KalmanFilter', center_only=False),
+    reid=dict(
+        type='BaseReID',
+        data_preprocessor=None,
+        backbone=dict(
+            type='mmcls.ResNet',
+            depth=50,
+            num_stages=4,
+            out_indices=(3, ),
+            style='pytorch'),
+        neck=dict(type='GlobalAveragePooling', kernel_size=(8, 4), stride=1),
+        head=dict(
+            type='LinearReIDHead',
+            num_fcs=1,
+            in_channels=2048,
+            fc_channels=1024,
+            out_channels=128,
+            num_classes=380,
+            loss_cls=dict(type='mmcls.CrossEntropyLoss', loss_weight=1.0),
+            loss_triplet=dict(type='TripletLoss', margin=0.3, loss_weight=1.0),
+            norm_cfg=dict(type='BN1d'),
+            act_cfg=dict(type='ReLU')),
+        init_cfg=dict(
+            type='Pretrained',
+            checkpoint=  # noqa: E251
+            'https://download.openmmlab.com/mmtracking/mot/reid/tracktor_reid_r50_iter25245-a452f51f.pth'  # noqa: E501
+        )),
+    tracker=dict(
+        type='SORTTracker',
+        obj_score_thr=0.5,
+        reid=dict(
+            num_samples=10,
+            img_scale=(256, 128),
+            img_norm_cfg=None,
+            match_score_thr=2.0),
+        match_iou_thr=0.5,
+        momentums=None,
+        num_tentatives=2,
+        num_frames_retain=100))
+
+train_dataloader = None
+
+train_cfg = None
+val_cfg = dict(type='ValLoop')
+test_cfg = dict(type='TestLoop')
